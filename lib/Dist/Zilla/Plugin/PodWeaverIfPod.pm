dbf03b2037006d6c202c67e28e4d986fea12dae5
Jonathan Swartz <swartz@pobox.com>
2012-04-27 03:35:36 +0000

    Import of JSWARTZ/Dist-Zilla-Plugin-PodWeaverIfPod-0.02 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-PodWeaverIfPod
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JSWARTZ/Dist-Zilla-Plugin-PodWeaverIfPod-0.02.tar.gz
    gitpan-cpan-author:       JSWARTZ
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/PodWeaverIfPod.pm
+++ lib/Dist/Zilla/Plugin/PodWeaverIfPod.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::Plugin::PodWeaverIfPod;
 BEGIN {
-  $Dist::Zilla::Plugin::PodWeaverIfPod::VERSION = '0.01';
+  $Dist::Zilla::Plugin::PodWeaverIfPod::VERSION = '0.02';
 }
 
 use Moose;
@@ -24,17 +24,13 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 1;
 
-
+__END__
 
 =pod
 
 =head1 NAME
 
 Dist::Zilla::Plugin::PodWeaverIfPod - Apply PodWeaver if there is already Pod
-
-=head1 VERSION
-
-version 0.01
 
 =head1 SYNOPSIS
 
@@ -46,20 +42,4 @@ In your L<Dist::Zilla> C<dist.ini>:
 
 Dist::Zilla::Plugin::PodWeaverIfPod will only PodWeaver a .pm if there appears
 to be Pod there (i.e. a C<=head1> section).
-
-=head1 AUTHOR
-
-Jonathan Swartz <swartz@pobox.com>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2011 by Jonathan Swartz.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
-=cut
-
-
-__END__
 
